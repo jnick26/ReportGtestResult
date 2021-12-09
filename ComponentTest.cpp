9cae4ec3cd8bb235c3b4ba5cf7820cf75c69b981
Mykhailo Diachenko <jnick26@gmail.com>
2021-12-09 21:55:20 +0200

    Add basic and basic failing tests

---
+++ ComponentTest.cpp
@@ -0,0 +1,11 @@
+#include <gmock/gmock.h>
+
+using testing::Eq;
+
+namespace {
+int add(int a, int b) { return a + b; }
+} // namespace
+
+TEST(ComponentTest, Basic) { EXPECT_THAT(add(10, 20), Eq(30)); }
+
+TEST(ComponentTest, FailingBasic) { EXPECT_THAT(add(10, 20), Eq(40)); }
